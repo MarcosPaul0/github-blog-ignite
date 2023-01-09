a684a54ae026f16b08a45b9da1f82204bbf9c5e1
MarcosPaul0 <marcosphip7@gmail.com>
2023-01-09 10:57:46 -0300

    feat: tema da aplicação

---
+++ src/themes/defaultTheme.ts
@@ -0,0 +1,24 @@
+export const defaultTheme = {
+  COLORS: {
+    BLUE: "#3294F8",
+
+    GRAY_50: "#E7EDF4",
+    GRAY_100: "#C4D4E3",
+    GRAY_200: "#AFC2D4",
+    GRAY_300: "#7B96B2",
+    GRAY_400: "#3A536B",
+    GRAY_500: "#1C2F41",
+    GRAY_600: "#112131",
+    GRAY_700: "#0B1B2B",
+    GRAY_800: "#071422",
+    GRAY_900: "#040F1A",
+  },
+  FONTS: {
+    XSM: '0.75rem',
+    SM: '0.875rem',
+    MD: '1rem',
+    LG: '1.125rem',
+    XLG: '1.25rem',
+    XXLG: '1.5rem',
+  }
+} as const;
