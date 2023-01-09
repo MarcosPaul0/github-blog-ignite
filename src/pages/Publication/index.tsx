5ff48240e1e57e078ac2d9a820f1826bb3f59717
MarcosPaul0 <marcosphip7@gmail.com>
2023-01-09 10:55:42 -0300

    feat: página publicação

---
+++ src/pages/Publication/index.tsx
@@ -0,0 +1,10 @@
+import { PublicationCard } from "./components/PublicationCard";
+import { PublicationContainer } from "./styles";
+
+export function Publication() {
+  return (
+    <PublicationContainer>
+      <PublicationCard />
+    </PublicationContainer>
+  );
+}
---
+++ src/pages/Publication/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const PublicationContainer = styled.main`
+  width: 100%;
+  max-width: 54rem;
+  padding: 0 1rem;
+`;
