f9d25276bd95909761bbd8de14e07075df4841c2
MarcosPaul0 <marcosphip7@gmail.com>
2023-01-09 10:54:48 -0300

    feat: componente cartão de publicação

---
+++ src/pages/Home/components/PublicationCard/styles.ts
@@ -0,0 +1,55 @@
+import { Link } from "react-router-dom";
+import styled, { css } from "styled-components";
+
+export const PublicationCardContainer = styled(Link)`
+  ${({ theme }) => css`
+    max-height: 16.25rem;
+    height: 100%;
+    padding: 2rem;
+    border-radius: 10px;
+    background: ${theme.COLORS.GRAY_600};
+    text-decoration: none;
+    transition: box-shadow 0.2s;
+
+    &:hover {
+      box-shadow: 0 0 0 2px ${theme.COLORS.GRAY_400};
+    }
+
+    p {
+      margin-top: 1.25rem;
+      width: 100%;
+      max-width: 26rem;
+      color: ${theme.COLORS.GRAY_200};
+      font-size: ${theme.FONTS.MD};
+      line-height: 160%;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-line-clamp: 4;
+      -webkit-box-orient: vertical;
+    }
+  `}
+`;
+
+export const TitleContainer = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    gap: 1rem;
+
+    h1 {
+      color: ${theme.COLORS.GRAY_50};
+      font-size: ${theme.FONTS.XLG};
+      font-weight: bold;
+      line-height: 160%;
+    }
+
+    time {
+      color: ${theme.COLORS.GRAY_300};
+      font-size: ${theme.FONTS.SM};
+      line-height: 160%;
+      white-space: nowrap;
+    }
+  `}
+`;
