48dade8731acbb9c49fb7043ee57803da440fc45
MarcosPaul0 <marcosphip7@gmail.com>
2023-01-10 09:27:00 -0300

    feat: funcionalidades da pagina home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,108 @@
 import { TextInput } from "../../components/TextInput";
+import { apiClient } from "../../services/apiClient";
 import { ProfileCard } from "./components/ProfileCard";
 import { PublicationCard } from "./components/PublicationCard";
-import { HomeContainer, PublicationsContainer, SearchContainer, SearchInfo } from "./styles";
+import {
+  HomeContainer,
+  PublicationsContainer,
+  SearchForm,
+  SearchInfo,
+} from "./styles";
+
+import { useEffect, useState } from "react";
+import { ApiRoutes } from "../../constants/apiRoutes";
+import { useForm } from "react-hook-form";
+import { Spinner } from "../../components/Spinner";
+
+interface Issue {
+  number: number;
+  title: string;
+  updated_at: string;
+  body: string;
+}
+
+interface Publication {
+  number: number;
+  title: string;
+  updatedAt: Date;
+  body: string;
+}
+
+interface SearchForm {
+  search: string;
+}
 
 export function Home() {
+  const [publications, setPublications] = useState<Publication[]>([]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchForm>({
+    defaultValues: {
+      search: "",
+    },
+  });
+
+  async function getGithubIssues({ search }: SearchForm) {
+    try {
+      const response = await apiClient.get(
+        `${ApiRoutes.SEARCH_ISSUES}?q=${search}repo:${
+          import.meta.env.VITE_GITHUB_BLOG_REPO
+        }`
+      );
+
+      const { items } = response.data;
+
+      setPublications(
+        items.map((item: Issue) => ({
+          number: item.number,
+          title: item.title,
+          updatedAt: new Date(item.updated_at),
+          body: item.body,
+        }))
+      );
+    } catch {
+      setPublications([]);
+    }
+  }
+
+  useEffect(() => {
+    getGithubIssues({ search: "" });
+  }, []);
+
   return (
     <HomeContainer>
       <ProfileCard />
 
-      <SearchContainer>
+      <SearchForm onSubmit={handleSubmit(getGithubIssues)}>
         <SearchInfo>
           <h1>Publicações</h1>
-          <span>0 publicações</span>
+          <span>{publications!.length} publicações</span>
         </SearchInfo>
 
-        <TextInput placeholder="Buscar conteúdo" />
-      </SearchContainer>
+        <TextInput placeholder="Buscar conteúdo" {...register("search")} />
+        <button type="submit" />
+      </SearchForm>
 
-      <PublicationsContainer>
-        <PublicationCard />
-        <PublicationCard />
-        <PublicationCard />
-        <PublicationCard />
-        <PublicationCard />
-      </PublicationsContainer>
+      {isSubmitting ? (
+        <Spinner />
+      ) : (
+        <PublicationsContainer>
+          {publications.map((publication) => (
+            <PublicationCard
+              key={publication.number}
+              publication={{
+                number: publication.number,
+                title: publication.title,
+                content: publication.body,
+                updatedAt: publication.updatedAt,
+              }}
+            />
+          ))}
+        </PublicationsContainer>
+      )}
     </HomeContainer>
   );
 }
